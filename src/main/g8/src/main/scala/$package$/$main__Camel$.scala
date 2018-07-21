093e2eceacb42029327cfa8161d85c1f26547c82
lettenj61 <lettenj61@users.noreply.github.com>
2018-07-22 07:38:20 +0900

    add 'sbtVersion' property

--- src/main/g8/src/main/scala/$package$/$main__Camel$.scala
+++ src/main/g8/src/main/scala/$package$/$main__Camel$.scala
@@ -1,10 +1,11 @@
 package $package$
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 import io.scalajs.nodejs
 
 @JSExportTopLevel("$main;format="Camel"$")
 object $main;format="Camel"$ {
-
   // write awesome things!
 }
